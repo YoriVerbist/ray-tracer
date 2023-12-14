6295dfc0efe585bec35db9d2c4633e07f4426420
Yori Verbist <yori.verbist12@gmail.com>
2023-12-14 20:24:23 +0100

    Started with ray tracer

--- src/InOneWeekend/ray.h
+++ src/InOneWeekend/ray.h
@@ -4,6 +4,7 @@
 #include "vec3.h"
 
 class ray {
+public:
   ray() {}
 
   ray(const point3 &origin, const vec3 &direction)
@@ -12,11 +13,11 @@ class ray {
   point3 origin() const { return orig; }
   vec3 direction() const { return dir; }
 
-  point3 at(doublte t) const { return orig + t * dir; }
+  point3 at(double t) const { return orig + t * dir; }
 
 private:
   point3 orig;
   vec3 dir;
-}
+};
 
 #endif // !RAY_H
