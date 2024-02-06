90c78d3e4fbaa5e0a4bbd5bf91fc5cbc9efd7fcc
Yori Verbist <yori.verbist12@gmail.com>
2024-02-06 22:23:02 +0100

    Started with recursive ray tracing

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -11,8 +11,10 @@
 
 class camera {
   public:
-    double aspect_ratio = 1.0; // Ratio of image width over height
-    int image_width = 100;     // Rendered image width in pixel count
+    double aspect_ratio = 1.0;  // Ratio of image width over height
+    int image_width = 100;      // Rendered image width in pixel count
+    int samples_per_pixel = 10; // Count of random samples for each pixel
+    int max_depth = 10;         // Max number of ray bounces into the scene
 
     void render(const hittable &world) {
         initialize();
@@ -23,17 +25,16 @@ class camera {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
                       << std::flush;
             for (int i = 0; i < image_width; ++i) {
-                auto pixel_center =
-                    pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto ray_direction = pixel_center - center;
-                ray r(center, ray_direction);
-
-                color pixel_color = ray_color(r, world);
-                write_color(std::cout, pixel_color);
+                color pixel_color(0, 0, 0);
+                for (int sample = 0; sample < samples_per_pixel; ++sample) {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, max_depth, world);
+                }
+                write_color(std::cout, pixel_color, samples_per_pixel);
             }
         }
 
-        std::clog << "\rDone.\n";
+        std::clog << "\rDone.                  \n";
     }
 
   private:
@@ -74,11 +75,38 @@ class camera {
             viewport_upper_left_corner + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-    color ray_color(const ray &r, const hittable &world) const {
-        hit_record vec;
+    ray get_ray(int i, int j) const {
+        // Get a randomly sampled camera ray for the pixel location i, j
 
-        if (world.hit(r, interval(0, infinity), vec)) {
-            return 0.5 * (vec.normal + color(1, 1, 1));
+        auto pixel_center =
+            pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+        auto pixel_sample = pixel_center + pixel_sample_square();
+
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 pixel_sample_square() const {
+        // Returns a random point in the square surrounding a pixel at the
+        // origin
+        auto px = -0.5 + random_double();
+        auto py = -0.5 + random_double();
+        return (px * pixel_delta_u) + (py * pixel_delta_v);
+    }
+
+    color ray_color(const ray &r, int depth, const hittable &world) const {
+        hit_record rec;
+
+        // If we've exceeded the ray bounce limit, no more light is gathered.
+        if (depth <= 0) {
+            return color(0, 0, 0);
+        }
+
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            vec3 direction = random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
