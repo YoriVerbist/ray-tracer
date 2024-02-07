d315a72a30267a0ff5d9f61df9f61a3dc1030359
Yori Verbist <yori.verbist12@gmail.com>
2024-02-07 20:51:36 +0100

    Added materials

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -5,6 +5,7 @@
 
 #include "color.h"
 #include "hittable.h"
+#include "material.h"
 
 #include <iostream>
 
@@ -104,8 +105,10 @@ class camera {
         hit_record rec;
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
