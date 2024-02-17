73b62d4656b641f9451b5e2be4988ac65b6b33a2
Yori Verbist <yori.verbist12@gmail.com>
2024-02-17 17:43:41 +0100

    Added bvh

--- src/aabb.h
+++ src/aabb.h
@@ -20,6 +20,12 @@ class aabb {
         z = interval(fmin(a[2], b[2]), fmax(a[2], b[2]));
     }
 
+    aabb(const aabb &box0, const aabb &box1) {
+        x = interval(box0.x, box1.x);
+        y = interval(box0.y, box1.y);
+        z = interval(box0.z, box1.z);
+    }
+
     const interval &axis(int n) const {
         if (n == 1)
             return y;
@@ -30,12 +36,20 @@ class aabb {
 
     bool hit(const ray &r, interval ray_t) const {
         for (int a = 0; a < 3; a++) {
-            auto t0 = fmin((axis(a).min - r.origin()[a]) / r.direction()[a],
-                           (axis(a).max - r.origin()[a]) / r.direction()[a]);
-            auto t1 = fmin((axis(a).min - r.origin()[a]) / r.direction()[a],
-                           (axis(a).max - r.origin()[a]) / r.direction()[a]);
-            ray_t.min = fmax(t0, ray_t.min);
-            ray_t.max = fmin(t1, ray_t.max);
+            auto invD = 1 / r.direction()[a];
+            auto origin = r.origin()[a];
+
+            auto t0 = (axis(a).min - origin) * invD;
+            auto t1 = (axis(a).max - origin) * invD;
+
+            if (invD < 0)
+                std::swap(t0, t1);
+
+            if (t0 > ray_t.min)
+                ray_t.min = t0;
+            if (t1 < ray_t.max)
+                ray_t.max = t1;
+
             if (ray_t.max <= ray_t.min)
                 return false;
         }
