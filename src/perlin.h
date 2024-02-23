f0549ab81702c974645b72f0a788cda0e5744ad5
Yori Verbist <yori.verbist12@gmail.com>
2024-02-23 22:47:11 +0100

    Added quads

--- src/perlin.h
+++ src/perlin.h
@@ -1,25 +1,14 @@
 #ifndef PERLIN_H
 #define PERLIN_H
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
 
 #include "rtweekend.h"
-
 
 class perlin {
   public:
     perlin() {
         ranvec = new vec3[point_count];
         for (int i = 0; i < point_count; ++i) {
-            ranvec[i] = unit_vector(vec3::random(-1,1));
+            ranvec[i] = unit_vector(vec3::random(-1, 1));
         }
 
         perm_x = perlin_generate_perm();
@@ -34,7 +23,7 @@ class perlin {
         delete[] perm_z;
     }
 
-    double noise(const point3& p) const {
+    double noise(const point3 &p) const {
         auto u = p.x() - floor(p.x());
         auto v = p.y() - floor(p.y());
         auto w = p.z() - floor(p.z());
@@ -43,19 +32,17 @@ class perlin {
         auto k = static_cast<int>(floor(p.z()));
         vec3 c[2][2][2];
 
-        for (int di=0; di < 2; di++)
-            for (int dj=0; dj < 2; dj++)
-                for (int dk=0; dk < 2; dk++)
-                    c[di][dj][dk] = ranvec[
-                        perm_x[(i+di) & 255] ^
-                        perm_y[(j+dj) & 255] ^
-                        perm_z[(k+dk) & 255]
-                    ];
+        for (int di = 0; di < 2; di++)
+            for (int dj = 0; dj < 2; dj++)
+                for (int dk = 0; dk < 2; dk++)
+                    c[di][dj][dk] =
+                        ranvec[perm_x[(i + di) & 255] ^ perm_y[(j + dj) & 255] ^
+                               perm_z[(k + dk) & 255]];
 
         return perlin_interp(c, u, v, w);
     }
 
-    double turb(const point3& p, int depth=7) const {
+    double turb(const point3 &p, int depth = 7) const {
         auto accum = 0.0;
         auto temp_p = p;
         auto weight = 1.0;
@@ -71,12 +58,12 @@ class perlin {
 
   private:
     static const int point_count = 256;
-    vec3* ranvec;
-    int* perm_x;
-    int* perm_y;
-    int* perm_z;
+    vec3 *ranvec;
+    int *perm_x;
+    int *perm_y;
+    int *perm_z;
 
-    static int* perlin_generate_perm() {
+    static int *perlin_generate_perm() {
         auto p = new int[point_count];
 
         for (int i = 0; i < point_count; i++)
@@ -87,9 +74,9 @@ class perlin {
         return p;
     }
 
-    static void permute(int* p, int n) {
-        for (int i = n-1; i > 0; i--) {
-            int target = random_int(0,i);
+    static void permute(int *p, int n) {
+        for (int i = n - 1; i > 0; i--) {
+            int target = random_int(0, i);
             int tmp = p[i];
             p[i] = p[target];
             p[target] = tmp;
@@ -97,23 +84,23 @@ class perlin {
     }
 
     static double perlin_interp(vec3 c[2][2][2], double u, double v, double w) {
-        auto uu = u*u*(3-2*u);
-        auto vv = v*v*(3-2*v);
-        auto ww = w*w*(3-2*w);
+        auto uu = u * u * (3 - 2 * u);
+        auto vv = v * v * (3 - 2 * v);
+        auto ww = w * w * (3 - 2 * w);
         auto accum = 0.0;
 
-        for (int i=0; i < 2; i++)
-            for (int j=0; j < 2; j++)
-                for (int k=0; k < 2; k++) {
-                    vec3 weight_v(u-i, v-j, w-k);
-                    accum += (i*uu + (1-i)*(1-uu))*
-                        (j*vv + (1-j)*(1-vv))*
-                        (k*ww + (1-k)*(1-ww))*dot(c[i][j][k], weight_v);
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++) {
+                    vec3 weight_v(u - i, v - j, w - k);
+                    accum += (i * uu + (1 - i) * (1 - uu)) *
+                             (j * vv + (1 - j) * (1 - vv)) *
+                             (k * ww + (1 - k) * (1 - ww)) *
+                             dot(c[i][j][k], weight_v);
                 }
 
         return accum;
     }
 };
-
 
 #endif
--- src/texture.h
+++ src/texture.h
@@ -80,4 +80,20 @@ class image_texture : public texture {
     rtw_image image;
 };
 
+class noise_texture : public texture {
+  public:
+    noise_texture() {}
+
+    noise_texture(double sc) : scale(sc) {}
+
+    color value(double u, double v, const point3 &p) const override {
+        auto s = scale * p;
+        return color(1, 1, 1) * 0.5 * (1 + sin(s.z() + 10 * noise.turb(s)));
+    }
+
+  private:
+    perlin noise;
+    double scale;
+};
+
 #endif
